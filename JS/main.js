87fd228dc77fa92ae9369b78518431f029c56611
NicolaTeora <nikxodia@gmail.com>
2024-01-31 11:07:19 +0100

    to do: stop show slide

--- JS/main.js
+++ JS/main.js
@@ -5,6 +5,7 @@ const app = createApp({
         return {
             slides: ['img/01.webp','img/02.webp','img/03.webp','img/04.webp','img/05.webp'],
             imgActive : 0,
+            slideActive : null,
         }
     },
 
@@ -26,13 +27,14 @@ const app = createApp({
         },
 
         showSlides(){
-            setInterval(()=>{
+            const slideActive = setInterval(()=>{
                 this.nextImg();
             }, 2000);
         },
 
         stopShowSlides(){
             // TODO: stop showSlides with mouseover
+            //clearInterval(this.slideActive)
         },
     },
     
